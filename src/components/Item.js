fbc03b328924241a75f82af9c9f3560a150ab67f
quinnshim2021 <quinnshim2021@u.northwestern.edu>
2021-06-01 00:38:39 -0500

    bug fixes

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
 import error from './../error.jpeg';
-import GoogleImageSearch from 'free-google-image-search'
+import loadingI from './../loading.gif';
 
 const Item = (id, order) => {
     const [anime, setAnime] = useState({});
     const [image, setImage] = useState(error);
+    const [loading, setLoading] = useState(true);
+    const loadingImage = loadingI;
 
+    /* get the new anime from the db */
     useEffect(() => {
         fetch(`https://anime-recommendator.herokuapp.com/anime/${id.id}`)
             .then(res => res.json())
@@ -18,20 +21,45 @@ const Item = (id, order) => {
             })
     }, [id]);
 
+    /* get the image for the anime */
     useEffect(() => {
+        setLoading(true);
         if (anime && anime.Title){
-            // get anime cover here
+            const words = anime.Title.split(" ");
+            let query = "";
+            words.map((word) => query += word + "+")
+            const BASEURL = "https://anime-recommendator.herokuapp.com/cover/";
+            // const BASEURL = "http://localhost:8081/cover/";
+            fetch(BASEURL + query)
+                .then(res => res.json())
+                .then((result) => {
+                    setImage(result);
+                    setLoading(false);
+                })
+                .catch(() => {
+                    setImage(error);
+                    setLoading(false);
+                })
         } else {
             setImage(error);
+            setLoading(false);
         }
     }, [anime])
 
     return (
       <div className="Item">
-        <img 
-            src={image}
-            alt="if you're seeing this then oops"
-            />
+          {
+              loading ?
+                <img 
+                    src={loadingImage}
+                    alt="if you're seeing this then oops"
+                    />
+              :
+              <img 
+                src={image}
+                alt="if you're seeing this then oops"
+                />
+          }
       </div>
     );
   }
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,5 +1,5 @@
 import Item from "./Item";
-import {useEffect, useState} from "react"
+import {useState} from "react"
 
 
 // to do:
